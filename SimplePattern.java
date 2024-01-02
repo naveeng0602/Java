149dd6d4ec217abd17beb1ddec31f65208a265cf
Naveen Gupta <141839367+naveeng0602@users.noreply.github.com>
2024-01-02 13:04:46 +0530

    Pattern

---
+++ SimplePattern.java
@@ -0,0 +1,32 @@
+public class SimplePattern {
+    public static void main(String[] args) {
+
+        // Squre//
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(j * j + " ");
+            }
+            System.out.println(" ");
+        }
+
+        // Cube//
+
+        // for(int i=1;i<=5;i++){
+        // for(int j=1;j<=6;j++) {
+        // System.out.print(j*j*j + " ");
+        // }
+        // System.out.println(" ");
+        // }
+
+        // Charcter//
+
+        // for(char i='F';i<='K';i++){
+        // for(char j='F';j<='K';j++) {
+        // System.out.print(j + " ");
+        // }
+        // System.out.println(" ");
+        // }
+
+    }
+}
